4e0abc369fb749cb9eb334e8aa6088ec18424f0c
Enrique Yañez Viñas <enyavi@gmail.com>
2026-04-20 22:53:15 +0200

    add: spring initializer

---
+++ src/main/java/es/enyavi/kash/KashApplication.java
@@ -0,0 +1,13 @@
+package es.enyavi.kash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/enyavi/kash/KashApplicationTests.java
@@ -0,0 +1,13 @@
+package es.enyavi.kash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
